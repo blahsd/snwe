72c5e45f636ac9f7835123c0013c91b6102789e0
blahsd <blahsd101@gmail.com>
2018-05-09 15:32:42 +0200

    Change behaviour of settings button. Clicking it a second time while the settinsg pane is open closes the pane. This addresses issue #9

--- app/js/windowManager.js
+++ app/js/windowManager.js
@@ -1,19 +1,50 @@
 const electron = require('electron');
 const BrowserWindow = electron.remote.BrowserWindow;
 const remove = require('electron').remote;
+var openWindows = {};
 
-function openPopWindow( windowpath ) {
-  const popWindow = new BrowserWindow({
+function removeFromArray(array, element) {
+    const index = array.indexOf(element);
 
+    if (index !== -1) {
+        array.splice(index, 1);
+    }
+}
+
+function isInArray(array, element) {
+  const index = array.indexOf(element);
+  if (index !== -1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function openPopWindow(windowpath) {
+  console.log(openWindows);
+  console.log(openWindows[windowpath]);
+
+  if (openWindows[windowpath] != null) {
+    openWindows[windowpath].close();
+    return;
+  }
+
+  popWindow = new BrowserWindow({
      frame: false,
-     transparent: true
+     transparent: true,
   });
 
   popWindow.loadURL('file://' + __dirname + '/' + windowpath);
+  openWindows[windowpath] = popWindow;
   //popWindow.webContents.openDevTools();
 
   popWindow.webContents.on("changeTheme", function(e) {
-    console.log("C-c-c-change!")
+    console.log("C-c-c-change!");
     loadSettings();
+  })
+
+  popWindow.webContents.on("close", function(e) {
+    console.log("C-c-c-closing!");
+    openWindows[windowpath] = null;
   })
 }
